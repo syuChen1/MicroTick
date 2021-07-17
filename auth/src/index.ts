cc608758d9dccdbd8ef7db82684d4efbf2382b60
Siyu Chen <siyuchen1@ufl.edu>
2021-07-17 10:27:33 -0400

    added error login & middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(json());
@@ -12,6 +13,8 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!!');
