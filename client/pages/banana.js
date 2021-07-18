b5a9222d202d8f81bd5142a1f322049b4c1b4f62
Siyu Chen <siyuchen1@ufl.edu>
2021-07-18 17:44:02 -0400

    added Next.js, Next image. Next k8s

---
+++ client/pages/banana.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const banana = () => {
+  return <h1>Banana</h1>;
+};
+
+export default banana;
---
+++ client/pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const index = () => {
+  return <h1>Landing Page</h1>;
+};
+
+export default index;
