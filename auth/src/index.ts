07fd42b03b21557d63b402957ddd98a4922d3f10
Siyu Chen <siyuchen1@ufl.edu>
2021-07-18 03:02:03 -0400

    added jwt env on pod && formating mongo res json

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -34,6 +34,10 @@ app.all('*', async () => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must ne defined');
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useNewUrlParser: true,
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -40,7 +40,7 @@ router.post(
         id: user.id,
         email: user.email,
       },
-      'asdf'
+      process.env.JWT_KEY!
     );
 
     // Store it on session object
