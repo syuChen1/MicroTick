48e0223eae1ff1c630a5ff8c0bd0311bc42be00f
Siyu Chen <siyuchen1@ufl.edu>
2021-07-18 12:43:28 -0400

    auth middleware

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,25 +1,11 @@
-import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
+import express from 'express';
 
 const router = express.Router();
 
-router.post(
-  '/api/users/signout',
-  [
-    body('email').isEmail().withMessage('Email must be valid'),
-    body('password')
-      .trim()
-      .notEmpty()
-      .withMessage('You must supply a password'),
-  ],
-  (req: Request, res: Response) => {
-    const errors = validationResult(req);
+router.post('/api/users/signout', (req, res) => {
+  req.session = null;
 
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-  }
-);
+  res.send({});
+});
 
 export { router as signoutRouter };
