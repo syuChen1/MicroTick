c8e59bf3c8b2dc505f2dafe1f9ae9a6c3bac1c14
syuchen1 <siyuchen1@ufl.edu>
2021-07-25 19:05:58 -0400

    durable subscription

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -15,10 +15,15 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('accouting-service');
+
   const subscription = stan.subscribe(
     'ticket:created',
-    'orders-service-queue-group',
+    'queue-geoup=name',
     options
   );
 
