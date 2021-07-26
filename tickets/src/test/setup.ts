e89b8610227fa22a565e715ed24fd375f56e06ce
syuchen1 <siyuchen1@ufl.edu>
2021-07-26 00:06:20 -0400

    added event tests && NATS env

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,6 +1,8 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 
+jest.mock('../nats-wrapper');
+
 let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = 'asdfghjk';
@@ -14,6 +16,7 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
+  jest.clearAllMocks();
   const collections = await mongoose.connection.db.collections();
 
   for (let collection of collections) {
