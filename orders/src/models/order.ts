77f630ec1d06223e73cf69371b5a6f59128749c9
syuchen1 <siyuchen1@ufl.edu>
2021-07-28 12:08:13 -0400

    apply version query for order ticket

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -15,6 +15,7 @@ interface OrderDoc extends mongoose.Document {
   userId: string;
   status: OrderStatus;
   expiresAt: Date;
+  version: number;
   ticket: TicketDoc;
 }
 
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -30,6 +30,7 @@ router.delete(
     // Publishing an event sating this was canceled
     new OrderCancelledPublisher(natsWrapper.client).publish({
       id: order.id,
+      version: order.version,
       ticket: {
         id: order.ticket.id,
       },
