2191e940f5ec27045028ea425247507cd000f152
syuchen1 <siyuchen1@ufl.edu>
2021-07-29 20:08:58 -0400

    added startup test

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,6 +7,7 @@ import { ExpirationCompleteListener } from './events/listeners/expiration-comple
 import { PaymentCreatedListener } from './events/listeners/payment-created-listener';
 
 const start = async () => {
+  console.log('testing...');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must ne defined');
   }
