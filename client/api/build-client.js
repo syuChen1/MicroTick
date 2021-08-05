2b8d43949f21fc74a83709dd0bc9b48ee5c43165
syuchen1 <siyuchen1@ufl.edu>
2021-08-05 12:37:37 -0400

    deploy client bug

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     // we are on the server!
-    let baseURL = 'http://www.microtick.xyz';
+    let baseURL = 'https://www.microtick.xyz';
     if (process.env.NODE_ENV === 'development') {
       baseURL =
         'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local';
